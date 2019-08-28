fe2c0e660395741b3abe4dc7eefe10a047115140
Derek Tingle <derek.tingle@gmail.com>
2019-08-28 22:57:55 +0200

    Uncomment run

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 # zsh
-#sudo apt-get install -y zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-#cp .tmux.conf ~/
+cp .tmux.conf ~/
 
-git clone git@github.com:vim/vim.git
+# git clone git@github.com:vim/vim.git
 
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#cp .vimrc ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/
 
